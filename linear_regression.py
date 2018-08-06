886079a82e9b9a0dae02eaeeca5e61eac2f20f4a
Shehz <m.shehz@gmail.com>
2018-08-06 15:00:02 +0530

    linear regression added

---
+++ linear_regression.py
@@ -0,0 +1,97 @@
+from __future__ import print_function,division
+
+import torch
+import matplotlib.pyplot as plt
+"""
+Linear Regression
+"""
+
+import numpy as np
+
+device = torch.device('cuda')
+dtype = torch.float
+
+
+np.random.seed(42)
+pts = 50
+
+"""
+#Randomly generated data
+
+x_vals = np.random.rand(2,50)
+x_train = np.asarray(x_vals,dtype=np.float32).reshape(-1,1)
+m = 1
+alpha = np.random.rand(1)
+beta = np.random.rand(1)
+y_correct = np.asarray([2*i+m for i in x_vals], dtype=np.float32).reshape(-1,1)
+"""
+
+def processingCsv(csv_path):
+    with open(csv_path) as csv_file:
+        csv_list = csv_file.readlines()
+
+        input_list = [value.strip().split(',') for value in csv_list]
+        x = []
+        y = []
+        for value in input_list[1:]:
+            x.append(float(value[0]))
+            y.append(float(value[1]))
+
+        x_array = np.asarray(x, dtype=np.float32).reshape(-1, 1)
+        y_array = np.asarray(y, dtype=np.float32).reshape(-1, 1)
+        return x_array,y_array
+
+class LinearRegression(object):
+    def __init__(self,learning_rate,w_dim,batch_size):
+        self.learning_rate = learning_rate
+        self.weight = torch.randn(1,w_dim, device=device, dtype=dtype, requires_grad=True)
+        self.bias = torch.randn(1, device=device, dtype=dtype, requires_grad=True)
+        self.batch_size = batch_size
+
+    def forward(self,x,y,batch_size):
+        y_pred =  self.weight*x + self.bias
+        loss = ((y_pred-y).pow(2).sum() ) / batch_size
+        return loss
+
+    def backward(self,loss):
+        loss.backward()
+        with torch.no_grad():
+            self.weight-=self.learning_rate * self.weight.grad
+            self.bias-= self.learning_rate * self.bias.grad
+            self.weight.grad.zero_()
+            self.bias.grad.zero_()
+
+    def predict(self,x):
+        print(self.weight, self.bias)
+        return x*self.weight + self.bias
+
+
+def run(csv_path,test_csv_path):
+    x_array,y_array =  processingCsv(csv_path)
+    xtest_array, ytest_array = processingCsv(test_csv_path)
+    num_epochs = 2000
+    learning_rate = 0.0001
+    batch_size =699
+    num_feature =1
+
+    linregression = LinearRegression(learning_rate,num_feature,batch_size)
+    for epoch  in range(num_epochs):
+        for idx in range(0,y_array.shape[0],batch_size):
+            x = torch.tensor(x_array[idx:batch_size+idx,:],device=device,dtype=dtype)
+            y = torch.tensor(y_array[idx:batch_size+idx,:],device=device,dtype=dtype)
+            loss= linregression.forward(x,y,batch_size)
+            linregression.backward(loss)
+        if (epoch+1) % 100 ==0:
+            print('Loss ',epoch+1, loss.item())
+
+    #testing
+
+    y_predict_test = linregression.predict(torch.tensor(xtest_array,device=device,dtype=dtype))
+    for idx in range(50):
+
+        print('Actual output is  {:f} ---> Predicted output is  {:f}'.format(y_predict_test[idx,0].item(), ytest_array[idx,0]))
+
+if __name__ =="__main__":
+    csv_path = "./data/regression/train_kaggle.csv"
+    test_csv_path ="./data/regression/test_kaggle.csv"
+    run(csv_path,test_csv_path)
